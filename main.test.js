116052e95a90b307ab29bcbfbe10b4881e5807dc
Michael Kuckuk <mdk@kuckuk.com>
2018-10-01 14:40:54 +0200

    Add tests for switchProfile and getRatingHistory

--- main.test.js
+++ main.test.js
@@ -5,10 +5,11 @@ const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 chai.use(chaiAsPromised);
 chai.use(sinonChai);
-// chai.use(sinon);
+
 const main = require('./main');
-const { waterfall, getProfileGuid } = main;
+const { waterfall, getProfileGuid, switchProfile, getRatingHistory, setRatingHistory } = main;
 const Netflix = require('netflix2');
+const fs = require('fs');
 
 describe('waterfall', () => {
 	let promises = [];
@@ -29,7 +30,7 @@ describe('waterfall', () => {
 		expect(waterfall([])).to.be.instanceOf(Promise);
 	});
 
-	it('Should execute all promises', (done, fail) => {
+	it('Should execute all promises', (done) => {
 		waterfall(promises)
 			.then(() => {
 				promises.forEach(promise => {
@@ -37,10 +38,10 @@ describe('waterfall', () => {
 				});
 			})
 			.then(done)
-			.catch(fail);
+			.catch(done);
 	});
 
-	it('Should execute all promises in correct order', (done, fail) => {
+	it('Should execute all promises in correct order', (done) => {
 		waterfall(promises)
 			.then(() => {
 				for (let i = 1; i < promises.length; i++) {
@@ -48,7 +49,7 @@ describe('waterfall', () => {
 				}
 				done();
 			})
-			.catch(fail);
+			.catch(done);
 	});
 });
 
@@ -98,14 +99,122 @@ describe('getProfileGuid', () => {
 	});
 });
 
-describe('', () => {
+describe('switchProfile', () => {
+	let netflix, netflixSwitchProfile;
+	const guid = {foo: 'bar'};
+	const result = {so: 'amazing'};
 
+	beforeEach(() => {
+		netflix = new Netflix();
+		netflixSwitchProfile = sinon.stub(netflix, 'switchProfile')
+			.returns(Promise.resolve(result));
+	});
+
+	it('Should return a promise', () => {
+		expect(switchProfile(netflix, guid)).to.be.instanceOf(Promise);
+	});
+
+	it('Should call netflix.switchProfile and return it\'s value', (done) => {
+		switchProfile(netflix, guid)
+			.then((res) => {
+				expect(netflixSwitchProfile).to.have.been.calledWithExactly(guid);
+				expect(res).to.deep.equal(result);
+				done();
+			});
+	});
 });
 
-describe('', () => {
+describe('getRatingHistory', () => {
+	let netflix, netflixGetRatingHistory, processStdoutWrite, fsWriteFileSync;
+	const filename = 'test.json';
+	const ratings = [
+		{
+			'ratingType': 'star',
+			'title': 'Some movie',
+			'movieID': 12345678,
+			'yourRating': 5,
+			'intRating': 50,
+			'date': '01/02/2016',
+			'timestamp': 1234567890123,
+			'comparableDate': 1234567890
+		},
+		{
+			'ratingType': 'thumb',
+			'title': 'Amazing Show',
+			'movieID': 87654321,
+			'yourRating': 2,
+			'date': '02/02/2018',
+			'timestamp': 2234567890123,
+			'comparableDate': 2234567890
+		}
+	];
 
-});
+	// JSON representations are hard coded into this test in order to notice when JSON.stringify changes
+	const ratingsJSON = '[{"ratingType":"star","title":"Some movie","movieID":12345678,"yourRating":5,"intRating":50,"date":"01/02/2016","timestamp":1234567890123,"comparableDate":1234567890},{"ratingType":"thumb","title":"Amazing Show","movieID":87654321,"yourRating":2,"date":"02/02/2018","timestamp":2234567890123,"comparableDate":2234567890}]';
+	const ratingsJSONWith4Spaces = '[\n    {\n        "ratingType": "star",\n        "title": "Some movie",\n        "movieID": 12345678,\n        "yourRating": 5,\n        "intRating": 50,\n        "date": "01/02/2016",\n        "timestamp": 1234567890123,\n        "comparableDate": 1234567890\n    },\n    {\n        "ratingType": "thumb",\n        "title": "Amazing Show",\n        "movieID": 87654321,\n        "yourRating": 2,\n        "date": "02/02/2018",\n        "timestamp": 2234567890123,\n        "comparableDate": 2234567890\n    }\n]';
 
-describe('', () => {
+	beforeEach(() => {
+		netflix = new Netflix();
+		netflixGetRatingHistory = sinon.stub(netflix, 'getRatingHistory')
+			.returns(Promise.resolve(ratings));
+		processStdoutWrite = sinon.stub(process.stdout, 'write');
+		fsWriteFileSync = sinon.stub(fs, 'writeFileSync');
+	});
 
+	afterEach(() => {
+		netflixGetRatingHistory.restore();
+		processStdoutWrite.restore();
+		fsWriteFileSync.restore();
+	});
+
+	it('Should return a promise', () => {
+		expect(getRatingHistory(netflix, filename, null)).to.be.instanceOf(Promise);
+	});
+
+	it('Should only print to process.stdout when filename is not specified', (done) => {
+		getRatingHistory(netflix, undefined, null)
+			.then(() => {
+				expect(processStdoutWrite).to.have.been.calledOnce;
+				expect(fsWriteFileSync).to.not.have.been.called;
+				done();
+			})
+			.catch(done);
+	});
+
+	it('Should only print to file when filename is specified', (done) => {
+		getRatingHistory(netflix, filename, null)
+			.then(() => {
+				expect(fsWriteFileSync).to.have.been.calledOnce;
+				expect(processStdoutWrite).to.not.have.been.called;
+				done();
+			})
+			.catch(done);
+	});
+
+	it('Should print correct JSON to process.stdout', (done) => {
+		getRatingHistory(netflix, undefined, null)
+			.then(() => {
+				expect(processStdoutWrite).to.have.been.calledOnceWith(ratingsJSON);
+				done();
+			})
+			.catch(done);
+	});
+
+	it('Should print correct JSON to file', (done) => {
+		getRatingHistory(netflix, filename, null)
+			.then(() => {
+				expect(fsWriteFileSync).to.have.been.calledOnceWith(filename, ratingsJSON);
+				done();
+			})
+			.catch(done);
+	});
+
+	it('Should print correct JSON when spaces is specified', (done) => {
+		getRatingHistory(netflix, filename, 4)
+			.then(() => {
+				expect(fsWriteFileSync).to.have.been.calledOnceWith(filename, ratingsJSONWith4Spaces);
+				done();
+			})
+			.catch(done);
+	});
 });
